b9213d1156ff04f509ab4bdc10541ac3e7889ac1
Jonas Larsson <jonas_larsson1@outlook.com>
2023-12-07 09:22:01 +0100

    new post form works

---
+++ dist/scripts/posts.js
@@ -0,0 +1,80 @@
+export const setPostsLocally = (postObjects) => {
+
+    const existingPosts = JSON.parse(localStorage.getItem('DummyJSONPosts') || '[]')
+
+    const newPosts = postObjects.filter((newPost) => {
+        return !existingPosts.some((existingPost) => existingPost.id === newPost.id)
+    })
+
+    const updatedPosts = [...existingPosts, ...newPosts]
+
+    localStorage.setItem('DummyJSONPosts', JSON.stringify(updatedPosts))
+}
+
+export const getPostsLocally = (postID) => {
+    const allPosts = JSON.parse(localStorage.getItem('DummyJSONPosts'))
+    if (postID) {
+        return [allPosts.find((post) => post.id === postID)] || null
+    } else {
+        return allPosts
+    }
+}
+
+export const fetchPosts = async () => {
+    try {
+      const response = await fetch('https://dummyjson.com/posts');
+  
+      if (!response.ok) {
+        throw new Error(`Request failed with status: ${response.status}`)
+      }
+  
+      const responseJSON = await response.json()
+      
+      return responseJSON
+  
+    } catch (error) {
+      console.error('Error in fetchPosts():', error);
+    }
+}
+
+export const displayPosts = (postObjects) => {
+    const postListElement = document.getElementById('posts-list')
+
+    for (let i = 0; i < postObjects.length; i++) {
+        const post = postObjects[i]
+
+
+        const postElement = document.createElement('li')
+        postElement.classList.add('post')
+        postElement.innerHTML = 
+
+            `
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+            `
+
+        const tagsElement = document.createElement('ul')
+        tagsElement.classList.add('tags') 
+        post.tags.forEach((tag) => {
+            const tagElement = document.createElement('li')
+            tagElement.innerText = `#${tag}`
+            tagsElement.append(tagElement)
+        })
+        
+        postElement.append(tagsElement) 
+        postListElement.append(postElement)
+    }
+
+}
+
+export const fetchAndDisplayPosts = () => {
+    fetchPosts()
+    .then(responseObject => {
+        setPostsLocally(responseObject.posts)
+
+        displayPosts(getPostsLocally())
+    })
+    .catch(error => {
+        console.error('Error in fetchAndDisplayPosts:', error)
+    })
+}
--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -1,4 +1,5 @@
 import { initDraggable } from './dragging.js';
+import { setPostsLocally, fetchAndDisplayPosts } from './posts.js'
 
 const postBubble = document.querySelector('.post-bubble')
 const crossBubble = document.querySelector('.cross-bubble')
@@ -17,93 +18,35 @@ postBubble.addEventListener('click', () => {
     postBubble.classList.toggle('fa-pencil')
     postBubble.classList.toggle('fa-xmark')
 })
+
 crossBubble.addEventListener('click', () => {
     toggleHidden(newPostContainer.id)
 })
 
 newPostForm.addEventListener('submit', event => {
     event.preventDefault()
-    console.log(event)
+
+    let postTitleElement = document.getElementById('newTitle')
+    let postBodyElement = document.getElementById('newBody')
+    let postTagsElement = document.getElementById('newTags')
+
+    let newPostObject = 
+        {
+            title: postTitleElement.value,
+            body: postBodyElement.value,
+            tags: postTagsElement.value.split(" ")
+        } 
+    
+
+    if (newPostObject.title && newPostObject.body) {
+        setPostsLocally([newPostObject])
+        fetchAndDisplayPosts()
+    } 
+
+    postTitleElement.value = ''
+    postBodyElement.value = ''
+    postTagsElement.value = ''
 })
 
-const setPostsLocally = (postObjects) => {
-    const existingPosts = JSON.parse(localStorage.getItem('DummyJSONPosts') || '[]')
-
-    const newPosts = postObjects.filter((newPost) => {
-        return !existingPosts.some((existingPost) => existingPost.id === newPost.id)
-    })
-
-    const updatedPosts = [...existingPosts, ...newPosts]
-
-    localStorage.setItem('DummyJSONPosts', JSON.stringify(updatedPosts))
-}
-
-const getPostsLocally = (postID) => {
-    const allPosts = JSON.parse(localStorage.getItem('DummyJSONPosts'))
-    if (postID) {
-        return [allPosts.find((post) => post.id === postID)] || null
-    } else {
-        return allPosts
-    }
-}
-
-const fetchPosts = async () => {
-    try {
-      const response = await fetch('https://dummyjson.com/posts');
-  
-      if (!response.ok) {
-        throw new Error(`Request failed with status: ${response.status}`)
-      }
-  
-      const responseJSON = await response.json()
-      
-      return responseJSON
-  
-    } catch (error) {
-      console.error('Error in fetchPosts():', error);
-    }
-}
-
-const displayPosts = (postObjects) => {
-    const postListElement = document.getElementById('posts-list')
-
-    for (let i = 0; i < postObjects.length; i++) {
-        const post = postObjects[i]
-
-
-        const postElement = document.createElement('li')
-        postElement.classList.add('post')
-        postElement.innerHTML = 
-
-            `
-            <h3>${post.title}</h3>
-            <p>${post.body}</p>
-            `
-
-        const tagsElement = document.createElement('ul')
-        tagsElement.classList.add('tags') 
-        post.tags.forEach((tag) => {
-            const tagElement = document.createElement('li')
-            tagElement.innerText = `#${tag}`
-            tagsElement.append(tagElement)
-        })
-        
-        postElement.append(tagsElement) 
-        postListElement.append(postElement)
-    }
-
-}
-
-const fetchAndDisplayPosts = () => {
-    fetchPosts()
-    .then(responseObject => {
-        setPostsLocally(responseObject.posts)
-
-        displayPosts(getPostsLocally())
-    })
-    .catch(error => {
-        console.error('Error in fetchAndDisplayPosts:', error)
-    })
-}
 
 fetchAndDisplayPosts()
